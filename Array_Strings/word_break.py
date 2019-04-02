1f9e6f9167dbf4900bfbc102966b70a1cb92df02
Steven Jimenez <stevenjimenez@Stevens-MacBook-Pro.local>
2019-04-02 16:07:59 -0400

    Word Break

--- Array_Strings/word_break.py
+++ Array_Strings/word_break.py
@@ -22,3 +22,19 @@ Example 3:
 Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
 Output: false
 '''
+
+'''
+Approach
+1. I will iterate through the wordDict, for each item in wordDict
+    1.1 Check if word is a substring in the input
+    1.2 If word is a substring I will strip that word from the input string
+    1.3 If word is not a substring I will return false
+'''
+def word_break(input, wordDict):
+    for word in wordDict:
+        if word not in input:
+            return False
+        startOfWord = input.find(word)
+        endOfWord = startOfWord + len(word)
+        input = input[:startOfWord] + input[endOfWord:]
+    return True
