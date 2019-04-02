72eae523964e1b9ac4d874e6109234eb28048469
Steven Jimenez <stevenjimenez@Stevens-MacBook-Pro.local>
2019-04-02 16:22:13 -0400

    Word Break WIP

--- Array_Strings/word_break.py
+++ Array_Strings/word_break.py
@@ -28,13 +28,22 @@ Approach
 1. I will iterate through the wordDict, for each item in wordDict
     1.1 Check if word is a substring in the input
     1.2 If word is a substring I will strip that word from the input string
-    1.3 If word is not a substring I will return false
+    1.3 If word is not a substring I will continue to see other words
+
+2. Try and rebuild the input string with the possible values in the word dict
+   take the first word in word dict, try and complete the word, if it fails move to the next word
+   2.1 first make sure that we keep count to how many letters are left, if word to try is greater than words left move to the next word
+   2.2 upon finding a solution return true
 '''
 def word_break(input, wordDict):
+    if not wordDict:
+        return False
+    wordDict = sorted(wordDict)
     for word in wordDict:
         if word not in input:
-            return False
-        startOfWord = input.find(word)
-        endOfWord = startOfWord + len(word)
-        input = input[:startOfWord] + input[endOfWord:]
-    return True
+            continue
+        while word in input:
+            startOfWord = input.find(word)
+            endOfWord = startOfWord + len(word)
+            input = input[:startOfWord] + input[endOfWord:]
+    return True if not input else False
