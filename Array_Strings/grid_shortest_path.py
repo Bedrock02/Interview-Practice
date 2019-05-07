dfe0b51708e402a005e516b13f59f18daf31320e
Bedrock02 <jimsteve91@gmail.com>
2019-05-07 08:32:16 -0400

    Refactor Shortest Path

--- Array_Strings/grid_shortest_path.py
+++ Array_Strings/grid_shortest_path.py
@@ -1,26 +1,25 @@
 def find_shortest_path(grid, startCord, endCord):
-    queue = [('', 0, startCord)]
+    queue = [('', startCord)]
     visited = {}
     while queue:
-        for candidate in range(len(queue)):
-            popped = queue.pop(0)
-            path, steps, cord = popped
-            if cord == endCord:
-                return (path, steps)
-            if cord in visited and visited[cord]:
-                continue
-            else:
-                visited[cord] = True
-            i, j = cord
-            # Neighbor Left
-            if j - 1 >= 0 and grid[i][j - 1] != 1:
-                queue.append((path + 'R', steps + 1, (i, j-1)))
-            # Neighbor Right
-            if j + 1 < len(grid[0]) and grid[i][j + 1] != 1:
-                queue.append((path + 'R', steps + 1, (i, j + 1)))
-            # Neighbor Up
-            if i - 1 >= 0 and grid[i - 1][j] != 1:
-                queue.append((path + 'U', steps + 1, (i - 1, j)))
-            # Neighbor Down
-            if i + 1 < len(grid) and grid[i + 1][j] != 1:
-                queue.append((path + 'D', steps + 1, (i + 1, j)))
+        popped = queue.pop(0)
+        path, cord = popped
+        if cord == endCord:
+            return (path, len(path))
+        if cord in visited and visited[cord]:
+            continue
+        else:
+            visited[cord] = True
+        i, j = cord
+        # Neighbor Left
+        if j - 1 >= 0 and grid[i][j - 1] != 1:
+            queue.append((path + 'R', (i, j-1)))
+        # Neighbor Right
+        if j + 1 < len(grid[0]) and grid[i][j + 1] != 1:
+            queue.append((path + 'R', (i, j + 1)))
+        # Neighbor Up
+        if i - 1 >= 0 and grid[i - 1][j] != 1:
+            queue.append((path + 'U', (i - 1, j)))
+        # Neighbor Down
+        if i + 1 < len(grid) and grid[i + 1][j] != 1:
+            queue.append((path + 'D', (i + 1, j)))
